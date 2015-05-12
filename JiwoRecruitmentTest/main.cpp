f9c055079cbddced671a1d94097dcd3360a4370b
Yixiao Huang <excel@139.com>
2015-05-13 05:09:25 +0800

    Initial Commit

---
+++ JiwoRecruitmentTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JiwoRecruitmentTest
+//
+//  Created by Excel on 15/5/13.
+//  Copyright (c) 2015年 Excel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
