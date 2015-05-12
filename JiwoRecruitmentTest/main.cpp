f2ad03234782d0edf83e39b4a0362aa884eac8a0
Yixiao Huang <excel@139.com>
2015-05-13 06:31:04 +0800

    编写编程要求

--- JiwoRecruitmentTest/main.cpp
+++ JiwoRecruitmentTest/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  JiwoRecruitmentTest
+//  JiwoRecruitmentTest 机窝开发小组招募测试
 //
 //  Created by Excel on 15/5/13.
 //  Copyright (c) 2015年 Excel. All rights reserved.
@@ -8,8 +8,24 @@
 
 #include <iostream>
 
+/*
+ 请编写一个函数，名为  VipFilter_你的姓名，如：VipFilter_Huangyixiao
+ 1、构造一个10万个手机号码组成的数组msisdnArray（手机号码），10万个数字全部由随机函数产生，
+ 要求为13开头的合法的手机号码，不必去重，不必排序。
+ 2、构造一个10个数字组成的数组vipMsisdnArray（VIP手机号码），10个数据全部由随机函数产生，
+ 要求为138开头的合法的手机号码，需去重。
+ 3、对数组vipMsisdnArray进行快速排序。
+ 4、统计msisdnArray中每一个vip号码出现的次数，打印出每次出现的位置。
+ 
+ 
+ 要求：unix下gc++编译运行通过，算法合理高效，结果输出正确
+ */
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    
     return 0;
-}
+};
+
